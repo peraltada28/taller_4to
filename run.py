9cf0f13b7ac8c6a0324d040170d103f52b69fb6a
hp_ry <hperalta028@gmail.com>
2022-11-19 19:03:53 -0300

    Guardar run.py

--- run.py
+++ run.py
@@ -1,1 +1,14 @@
 print("Hola mundo Nro 1")
+
+from app import app
+
+#decorador
+#endpoint
+
+@app.route("/")
+def hello_world():
+    return "<p>Mi primer hello word con flask</p>"
+
+if __name__ == "master":
+    app.run(debug = True)
+
